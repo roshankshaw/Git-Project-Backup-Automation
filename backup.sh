0d3af7b65df9bf3db7f90052704276d022409aa8
roshankshaw <roshankushaw1@gmail.com>
2020-09-13 20:22:28 +0530

    Automate commit at 20:22:28

--- backup.sh
+++ backup.sh
@@ -1,3 +1,10 @@
 cd /home/roshan/Projects/Git-Project-Backup-Automation/
-git add .
-git commit -m "Automate commit"
+
+TEST=$(git status --porcelain|wc -l)
+if [ 0 -eq $TEST ]; then
+   echo "No changes"
+else
+   now=$(date +"%T")
+   git add .
+   git commit -m "Automate commit at $now"
+fi 
